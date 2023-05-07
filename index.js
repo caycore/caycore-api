cdd1479061674c304dd3c0ed8b21db5f06256213
lfabl <met2net3@gmail.com>
2023-05-07 21:23:47 +0300

    First commit.

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var cors = require("cors");
+var r = require('rethinkdbdash')({
+    servers: [
+        {
+            host: 'localhost',
+            port: 28015
+        }
+    ]
+});
+
+var app = express();
+
+app.use(cors());
+
+app.get("/getTeam", async (req, res) => {
+    r.db("caycore").table("team").then((team) => {
+        res.statusCode = 200;
+        res.send({
+            message: "Fetch success.",
+            code: 200,
+            payload: team
+        });
+    }).catch((err) => {
+        res.statusCode = 500;
+        res.send({
+            message: err.message,
+            code: 500,
+            payload: err
+        });
+    });
+});
+
+app.listen(3000);
